29b2b311e1d35cd72538a673cca59531cae11ffb
erick <henrique.mito16@gmail.com>
2025-10-27 10:29:41 -0300

    fix filter

--- src/components/MDR-filter.tsx
+++ src/components/MDR-filter.tsx
@@ -1,13 +1,13 @@
 import { FunnelIcon } from "@phosphor-icons/react"
 import { useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom"
-import type {MDRFilterType } from "../types/MRD"
+import type { MDRFilterType } from "../types/MRD"
 import { getFilterMDR } from "../api/get-filter-mdr"
 
 export function MDRFilter() {
 
   const [searchParams, setSearchParams] = useSearchParams()
-  
+
   const [filter, setFilter] = useState<MDRFilterType[]>()
   const [search, setSearch] = useState('')
   const [unidade, setUnidade] = useState(searchParams.get('unidade') || '')
@@ -15,11 +15,11 @@ export function MDRFilter() {
   const [processo, setProcesso] = useState(searchParams.get('processo') || '')
 
   const areaList = filter?.find((item) => item.unidade === unidade)?.area
-  const processoList = areaList?.find(({name}) => name === area)?.processo
+  const processoList = areaList?.find(({ name }) => name === area)?.processo
 
   function changeSearchParams() {
     setSearchParams((params) => {
-      if(unidade && area && processo) {
+      if (unidade && area && processo) {
         params.set('unidade', unidade)
         params.set('area', area)
         params.set('processo', processo)
@@ -30,45 +30,47 @@ export function MDRFilter() {
   }
 
   async function getFilter() {
-    const filterData = await  getFilterMDR()
+    const filterData = await getFilterMDR()
     setFilter(filterData)
   }
 
   useEffect(() => {
-    getFilter() 
+    getFilter()
   }, [])
 
   useEffect(() => {
-    if(!areaList?.length) {
+    if (searchParams.get("area")) return
+
+    if (!areaList?.length) {
       setArea("")
       return
     }
     setArea(areaList[0].name)
-  }, [areaList])
+  }, [areaList, searchParams])
 
   useEffect(() => {
-    if(!processoList?.length) {
+    if (!processoList?.length) {
       setProcesso("")
       return
     }
     setProcesso(processoList[0])
   }, [processoList])
 
-  console.log({areaList, area, unidade})
 
   return (
     <div className="flex gap-4 rounded-lg bg-white shadow-md p-4 mb-6 items-end flex-wrap">
       <div className="w-full flex flex-wrap sm:flex-row flex-col sm:items-end gap-4">
         <div className="flex-1 sm:basis-64">
           <label htmlFor="unidade-select" className="text-sm font-medium text-gray-700 mb-1">Unidade</label>
-          <select 
+          <select
             id="unidade-select"
-            name="Unidade" 
+            name="Unidade"
             className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 "
             onChange={(e) => setUnidade(e.target.value)}
+            defaultValue={unidade}
             value={unidade}
           >
-            {filter?.map(({unidade}) => (
+            {filter?.map(({ unidade }) => (
               <option value={unidade} key={unidade}>{unidade}</option>
             ))}
             <option value="" disabled>Selecione</option>
@@ -77,11 +79,12 @@ export function MDRFilter() {
 
         <div className="flex-1 sm:basis-64">
           <label htmlFor="area-select" className="text-sm font-medium text-gray-700 mb-1">Area</label>
-          <select 
+          <select
             id="area-select"
-            name="Area" 
+            name="Area"
             className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 "
             onChange={(e) => setArea(e.target.value)}
+            defaultValue={area}
             value={area}
           >
             {areaList?.map(area => (
@@ -93,11 +96,12 @@ export function MDRFilter() {
 
         <div className="flex-1 sm:basis-64">
           <label htmlFor="processo-select" className="text-sm font-medium text-gray-700 mb-1">Processo</label>
-          <select 
+          <select
             id="processo-select"
-            name="Processo" 
+            name="Processo"
             className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             onChange={(e) => setProcesso(e.target.value)}
+            defaultValue={processo}
             value={processo}
           >
             {processoList?.map(processo => (
@@ -106,11 +110,11 @@ export function MDRFilter() {
             <option value="" disabled>Selecione</option>
           </select>
         </div>
-        
+
         <div className="flex-1 sm:basis-64">
           <label className="text-sm font-medium text-gray-700 mb-1">Pesquisa</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 "
             value={search}
             onChange={e => setSearch(e.target.value)}
@@ -118,14 +122,14 @@ export function MDRFilter() {
           />
         </div>
 
-        <button 
+        <button
           className="h-10 flex items-center gap-2 bg-blue-600 hover:bg-blue-700 text-white text-sm font-medium px-4 rounded-md transition-colors cursor-pointer disabled:hover:bg-blue-600 disabled:opacity-70 disabled:cursor-not-allowed"
           onClick={changeSearchParams}
           disabled={!unidade || !area || !processo}
         >
-          <FunnelIcon size={20} weight="fill"/>
+          <FunnelIcon size={20} weight="fill" />
           Aplicar Filtros
-      </button>
+        </button>
       </div>
 
 
--- src/pages/submit-data/fileSubmit.tsx
+++ src/pages/submit-data/fileSubmit.tsx
@@ -1,122 +1,135 @@
-import { CheckIcon, CircleNotchIcon, FileArrowUpIcon, FileIcon, UploadSimpleIcon } from "@phosphor-icons/react"
-import { useState } from "react"
-import { getDataFromSpreadsheet } from "./getDataFromSpreadsheet"
-import { changeObjNamesInMDRList } from "./changeObjNamesInMDRList"
-import { formatedMDR } from "./formatedMDR"
-import type { MDR } from "../../types/MRD"
-import { toast } from "react-toastify"
-import { api } from "../../lib/axios"
+import {
+	CheckIcon,
+	CircleNotchIcon,
+	FileArrowUpIcon,
+	FileIcon,
+	UploadSimpleIcon,
+} from "@phosphor-icons/react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../../lib/axios";
+import type { MDR } from "../../types/MRD";
+import { changeObjNamesInMDRList } from "./changeObjNamesInMDRList";
+import { formatedMDR } from "./formatedMDR";
+import { getDataFromSpreadsheet } from "./getDataFromSpreadsheet";
 
 interface FileSubmitProps {
-  file: File
+	file: File;
 }
 
-export function FileSubmit({ file } : FileSubmitProps) {
-  const [loadingFile, setLoadingFile] = useState(false)
-  const [MDR, setMDR] = useState<MDR[]>()
-  const [unidade, setUnidade] = useState('Industrial Imperatriz')
-  const [area, setArea] = useState('')
-  const [processo, setProcesso] = useState('')
-  const sectors = { unidade, area, processo }
+export function FileSubmit({ file }: FileSubmitProps) {
+	const [loadingFile, setLoadingFile] = useState(false);
+	const [MDR, setMDR] = useState<MDR[]>();
+	const [unidade, setUnidade] = useState("Industrial Imperatriz");
+	const [area, setArea] = useState("");
+	const [processo, setProcesso] = useState("");
+	const sectors = { unidade, area, processo };
 
-  function loadFile() {
-    const reader = new FileReader()
+	function loadFile() {
+		const reader = new FileReader();
 
-    reader.onload = (e) => {
-      const dataSheet = getDataFromSpreadsheet(e)
-      const MDRUnformated = changeObjNamesInMDRList(dataSheet)
-      const MDRsFormatted = formatedMDR(MDRUnformated, sectors)
+		reader.onload = (e) => {
+			const dataSheet = getDataFromSpreadsheet(e);
+			const MDRUnformated = changeObjNamesInMDRList(dataSheet);
+			const MDRsFormatted = formatedMDR(MDRUnformated, sectors);
 
-      setMDR(MDRsFormatted)
-    }
+			setMDR(MDRsFormatted);
+		};
 
-    reader.readAsArrayBuffer(file)
-    reader.onloadstart = () => setLoadingFile(true)
-    reader.onloadend = () => setLoadingFile(false)
-  }
+		reader.readAsArrayBuffer(file);
+		reader.onloadstart = () => setLoadingFile(true);
+		reader.onloadend = () => setLoadingFile(false);
+	}
 
-  async function submitMDR() {
-    if(!MDR?.length) return
-    try {    
-      await api.post(`/MDR/create`, MDR)
-      toast.success(`${file.name} enviado com sucesso`)
-    } catch (err) {
-      console.error(err)
-      toast.error(`Erro no upload do arquivo ${file.name}`)
-    }
-  }
+	async function submitMDR() {
+		if (!MDR?.length) return;
+		try {
+			await api.post(`/MDR/create`, MDR);
+			toast.success(`${file.name} enviado com sucesso`);
+		} catch (err) {
+			console.error(err);
+			toast.error(`Erro no upload do arquivo ${file.name}`);
+		}
+	}
 
-  async function submitSectors() {
-    try {
-      const sector = {
-        sectorUnidade: unidade,
-        sectorArea: area,
-        sectorProcesso: processo
-      }
+	async function submitSectors() {
+		try {
+			const sector = {
+				sectorUnidade: unidade,
+				sectorArea: area,
+				sectorProcesso: processo,
+			};
 
-      await api.post(`/sector/create`, sector)
-    
-      toast.success(`Setores criados com sucesso`)
-    } catch (err) {
-      console.error(err)
-      toast.error(`Erro ao criar Setor`)
-    }
-  }
+			await api.post(`/sector/create`, sector);
 
-    return (
-      <div className="hover:shadow hover:shadow-black/30 rounded transition-all p-2">
-       <div className="flex items-center gap-2 text-purple-500" >
-        <FileIcon size={24} weight="fill"/>
-          <span>{file.name}</span>
-          {loadingFile ? (
-            <span className="animate-spin">
-              <CircleNotchIcon size={24}/>
-            </span>
-          ) : (
-            <span>
-              <CheckIcon size={24} weight="bold"/>
-            </span>
-          )}
-          {MDR?.length && (
-            <span>MDRs: {MDR.length}</span>
-          )}
-          <div className="ml-auto">
-            <button onClick={loadFile}>
-              <FileArrowUpIcon size={24} className="size-10 p-2 rounded text-emerald-500 cursor-pointer hover:bg-emerald-500 hover:text-white transition-colors"/>
-            </button>
+			toast.success(`Setores criados com sucesso`);
+		} catch (err) {
+			console.error(err);
+			toast.error(`Erro ao criar Setor`);
+		}
+	}
 
-            <button onClick={submitMDR}>
-              <UploadSimpleIcon size={24} className="size-10 p-2 rounded text-emerald-500 cursor-pointer hover:bg-emerald-500 hover:text-white transition-colors"/>
-            </button>
-          </div>
-        </div>
+	return (
+		<div className="hover:shadow hover:shadow-black/30 rounded transition-all p-2">
+			<div className="flex items-center gap-2 text-purple-500">
+				<FileIcon size={24} weight="fill" />
+				<span>{file.name}</span>
+				{loadingFile ? (
+					<span className="animate-spin">
+						<CircleNotchIcon size={24} />
+					</span>
+				) : (
+					<span>
+						<CheckIcon size={24} weight="bold" />
+					</span>
+				)}
+				{MDR?.length && <span>MDRs: {MDR.length}</span>}
+				<div className="ml-auto">
+					<button onClick={loadFile}>
+						<FileArrowUpIcon
+							size={24}
+							className="size-10 p-2 rounded text-emerald-500 cursor-pointer hover:bg-emerald-500 hover:text-white transition-colors"
+						/>
+					</button>
 
-        <div className="flex gap-4 text-yellow-900">
-          <input 
-            type="text" 
-            className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 "
-            value={unidade}
-            onChange={e => setUnidade(e.target.value)}
-            placeholder="unidade"
-          />
-          <input 
-            type="text" 
-            className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 "
-            value={area}
-            onChange={e => setArea(e.target.value)}
-            placeholder="area"
-          />
-          <input 
-            type="text" 
-            className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 "
-            value={processo}
-            onChange={e => setProcesso(e.target.value)}
-            placeholder="processo"
-          />
-          <button onClick={submitSectors}>
-              <UploadSimpleIcon size={24} className="size-10 p-2 rounded text-yellow-500 cursor-pointer hover:bg-yellow-500 hover:text-white transition-colors"/>
-            </button>
-        </div>
-      </div>
-    )
+					<button onClick={submitMDR}>
+						<UploadSimpleIcon
+							size={24}
+							className="size-10 p-2 rounded text-emerald-500 cursor-pointer hover:bg-emerald-500 hover:text-white transition-colors"
+						/>
+					</button>
+				</div>
+			</div>
+
+			<div className="flex gap-4 text-yellow-900">
+				<input
+					type="text"
+					className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 "
+					value={unidade}
+					onChange={(e) => setUnidade(e.target.value)}
+					placeholder="unidade"
+				/>
+				<input
+					type="text"
+					className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 "
+					value={area}
+					onChange={(e) => setArea(e.target.value)}
+					placeholder="area"
+				/>
+				<input
+					type="text"
+					className="w-full mt-1 block border border-gray-300 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-yellow-500 "
+					value={processo}
+					onChange={(e) => setProcesso(e.target.value)}
+					placeholder="processo"
+				/>
+				<button onClick={submitSectors}>
+					<UploadSimpleIcon
+						size={24}
+						className="size-10 p-2 rounded text-yellow-500 cursor-pointer hover:bg-yellow-500 hover:text-white transition-colors"
+					/>
+				</button>
+			</div>
+		</div>
+	);
 }
